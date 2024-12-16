0415b7cf4a561fba0ecc5d7966c6eed20b09076e
grqx_win <user.email>
2024-12-16 22:11:23 +1300

    rename "media" -> "resources"

--- extraLarge.pack.sh
+++ extraLarge.pack.sh
@@ -4,6 +4,6 @@ cd $(dirname "$0")
 PROJECT_NAME="extraLarge" \
 MAIN_CLASS="extraLarge.sustainPlusPlus" \
 EXTRA_DEPS_SRC=$(find "src/music_player" -name "*.java") \
-RSRC="media/extraLarge.wav" \
+RSRC="resources/extraLarge.wav" \
 RSRC_DEST_DIR="resources/" \
 devscripts/build_jar.sh
--- goddrinksjava.pack.sh
+++ goddrinksjava.pack.sh
@@ -4,6 +4,6 @@ cd $(dirname "$0")
 PROJECT_NAME="goddrinksjava" \
 MAIN_CLASS="goddrinksjava.GodDrinksJava" \
 EXTRA_DEPS_SRC=$(find "src/music_player" -name "*.java") \
-RSRC="media/goddrinksjava.wav" \
+RSRC="resources/goddrinksjava.wav" \
 RSRC_DEST_DIR="resources/" \
 devscripts/build_jar.sh
